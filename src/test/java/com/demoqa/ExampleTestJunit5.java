e83f9a97596419de149d3c42813b1b2616f261c6
Andrey Mozerov <andrey-mozerov@mail.ru>
2022-11-17 07:10:43 +0300

    Before After ALL

---
+++ src/test/java/com/demoqa/ExampleTestJunit5.java
@@ -0,0 +1,29 @@
+package com.demoqa;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import java.sql.SQLOutput;
+
+public class ExampleTestJunit5 {
+
+    @BeforeAll
+    static void configure(){
+        System.out.println("H");
+    }
+
+    @AfterAll
+    static void configure("B"){
+        System.out.println();
+    }
+
+    @Test
+    void test() {
+        Assertions.assertTrue(3>2);
+    }
+
+
+
+}
