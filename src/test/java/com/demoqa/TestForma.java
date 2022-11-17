412f5ea4004600c5534bdea2db76598677514305
Andrey Mozerov <andrey-mozerov@mail.ru>
2022-11-17 22:40:01 +0300

    TestForma add

--- src/test/java/com/demoqa/TestForma.java
+++ src/test/java/com/demoqa/TestForma.java
@@ -1,4 +1,23 @@
 package com.demoqa;
 
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selenide.open;
+
 public class TestForma {
+
+    @BeforeAll
+    static void configure(){
+        Configuration.baseUrl="https://demoqa.com";
+        Configuration.timeout= 10000;
+        Configuration.browserSize="1920*1080";
+        Configuration.browser="chrome";
+    }
+
+    @Test
+    void fillFormTest() {
+        open("/text-box");
+    }
 }
