c785b841ef67b241dfa4196312c7021ef6e02a54
Andrey Mozerov <andrey-mozerov@mail.ru>
2022-11-18 07:26:19 +0300

    TestForma all

--- src/test/java/com/demoqa/TestForma.java
+++ src/test/java/com/demoqa/TestForma.java
@@ -4,20 +4,30 @@ import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
 public class TestForma {
 
     @BeforeAll
     static void configure(){
-        Configuration.baseUrl="https://demoqa.com";
-        Configuration.timeout= 10000;
-        Configuration.browserSize="1920*1080";
+        Configuration.baseUrl="https://demoqa.com/";
+        Configuration.timeout= 1000;
         Configuration.browser="chrome";
+        Configuration.browserSize="1920*1080";
+
     }
 
     @Test
     void fillFormTest() {
         open("/text-box");
+        $("#UserName").setValue("Andrey");
+        $("#UserEmail").setValue("Andrey@mail.ru");
+        $("#currrentAddress").setValue("A");
+        $("#permanentAddress").setValue("S");
+        $("#submit").click();
+
+
+        $("#submit").click();
     }
 }
